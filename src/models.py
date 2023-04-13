5f9f59d4185245880023555cf408a29b626fdd34
[RonnieJohnston] <97621743+RonnieJohnston@users.noreply.github.com>
2023-04-13 13:33:01 -0400

    Update models.py

--- src/models.py
+++ src/models.py
@@ -3,3 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 #TODO: Implement ER Diagram tables here
+class Professor(db.Model):
+    professor_id = db.Column(db.Integer, primary_key=True)
+    department_id = db.Column(db.Integer, nullable=False)
+    first_name = db.Column(db.String(255), nullable=False)
+    last_name = db.Column(db.String(255), nullable=False)
+
