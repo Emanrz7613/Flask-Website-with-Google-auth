e994642276be04de1c7f922e51e58267e55bd83f
[RonnieJohnston] <97621743+RonnieJohnston@users.noreply.github.com>
2023-04-13 13:45:41 -0400

    fixed naming convention

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, abort, redirect, render_template, request
 
-from src.repositories.review_repository import review_repository_singleton
+from src.repositories.rating_repository import rating_repository_singleton
 
 from src.models import db
 
@@ -17,12 +17,16 @@ def index():
     return render_template('index.html')
 
 
-@app.get('/reviews')
-def list_all_reviews():
-    all_reviews = review_repository_singleton.get_all_reviews()
-    return render_template('list_all_reviews.html', list_reviews_active=True, reviews=all_reviews)
+@app.get('/ratings')
+def list_all_ratings():
+    all_ratings = rating_repository_singleton.get_all_ratings()
+    return render_template('list_all_ratings.html', list_ratings_active=True, ratings=all_ratings)
 
 #TODO: Implement more functions as we get html framework
+@app.get('/ratings/<int:movie_id>')
+def get_single_movie(movie_id):
+    single_movie = movie_repository_singleton.get_movie_by_id(movie_id)
+    return render_template('get_single_movie.html', movie=single_movie)
 
 if __name__ == '__main__':
     app.run(debug=True)
