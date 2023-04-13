7d37fe5759dd3d4086a7e2fe8bd4e94db575d9a2
Emanrz7613 <echaves@uncc.edu>
2023-04-12 20:05:40 -0400

    Sprint1 Docs
    add Sprint1 Docs and update ERD and Schema

--- Sprint_1/database_schema.sql
+++ Sprint_1/database_schema.sql
@@ -18,13 +18,29 @@ CREATE SCHEMA IF NOT EXISTS `3155_final_project` DEFAULT CHARACTER SET utf8mb4 C
 USE `3155_final_project` ;
 
 -- -----------------------------------------------------
+-- Table `3155_final_project`.`courses`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `3155_final_project`.`courses` (
+  `course_id` INT NOT NULL AUTO_INCREMENT,
+  `subject` CHAR(4) NOT NULL,
+  `course_num` CHAR(4) NOT NULL,
+  `course_title` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`course_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
 -- Table `3155_final_project`.`departments`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `3155_final_project`.`departments` (
   `department_id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL,
+  `name` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`department_id`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
@@ -39,22 +55,10 @@ CREATE TABLE IF NOT EXISTS `3155_final_project`.`professors` (
   INDEX `department_id_idx` (`deparment_id` ASC) VISIBLE,
   CONSTRAINT `department_id`
     FOREIGN KEY (`deparment_id`)
-    REFERENCES `3155_final_project`.`departments` (`department_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `3155_final_project`.`courses`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `3155_final_project`.`courses` (
-  `course_id` INT NOT NULL AUTO_INCREMENT,
-  `subject` CHAR(4) NOT NULL,
-  `course_num` CHAR(4) NOT NULL,
-  `course_title` VARCHAR(50) NOT NULL,
-  PRIMARY KEY (`course_id`))
-ENGINE = InnoDB;
+    REFERENCES `3155_final_project`.`departments` (`department_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
@@ -65,17 +69,15 @@ CREATE TABLE IF NOT EXISTS `3155_final_project`.`professor_course` (
   `course_id` INT NOT NULL,
   PRIMARY KEY (`professor_id`, `course_id`),
   INDEX `course_id_idx` (`course_id` ASC) VISIBLE,
-  CONSTRAINT `professor_id`
-    FOREIGN KEY (`professor_id`)
-    REFERENCES `3155_final_project`.`professors` (`professor_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
   CONSTRAINT `course_id`
     FOREIGN KEY (`course_id`)
-    REFERENCES `3155_final_project`.`courses` (`course_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `3155_final_project`.`courses` (`course_id`),
+  CONSTRAINT `professor_id`
+    FOREIGN KEY (`professor_id`)
+    REFERENCES `3155_final_project`.`professors` (`professor_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
@@ -89,7 +91,9 @@ CREATE TABLE IF NOT EXISTS `3155_final_project`.`users` (
   `password` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
@@ -99,23 +103,28 @@ CREATE TABLE IF NOT EXISTS `3155_final_project`.`ratings` (
   `rating_id` INT NOT NULL AUTO_INCREMENT,
   `user_id` INT NOT NULL,
   `course_id` INT NOT NULL,
+  `professor_id` INT NOT NULL,
   `rating` INT NOT NULL,
   `semester` VARCHAR(12) NOT NULL,
-  `comments` VARCHAR(255) NULL,
+  `comments` VARCHAR(255) NULL DEFAULT NULL,
   PRIMARY KEY (`rating_id`),
   INDEX `user_id_idx` (`user_id` ASC) VISIBLE,
   INDEX `course_id_idx` (`course_id` ASC) VISIBLE,
-  CONSTRAINT `user_id`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `3155_final_project`.`users` (`user_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+  INDEX `professor_idx` (`professor_id` ASC) VISIBLE,
   CONSTRAINT `course`
     FOREIGN KEY (`course_id`)
-    REFERENCES `3155_final_project`.`courses` (`course_id`)
+    REFERENCES `3155_final_project`.`courses` (`course_id`),
+  CONSTRAINT `user_id`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `3155_final_project`.`users` (`user_id`),
+  CONSTRAINT `professor`
+    FOREIGN KEY (`professor_id`)
+    REFERENCES `3155_final_project`.`professors` (`professor_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
