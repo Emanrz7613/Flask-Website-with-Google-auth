3513415f94abab20ef36fd52b83abc31ff9c11fb
iansyork <iansyork@outlook.com>
2023-04-20 17:36:29 -0400

    Joined Professors, Ratings, Courses in  get_rating_by_id method, began displaying single entry information

--- src/repositories/rating_repository.py
+++ src/repositories/rating_repository.py
@@ -11,7 +11,11 @@ class RatingRepository:
         return ratings
     
     def get_rating_by_id(self, rating_id):
-        single_rating = Ratings.query.get(rating_id)
+        single_rating = db.session.query(Ratings, Professors.first_name, Professors.last_name, Courses.course_num, Courses.subject, Ratings.rating_id, Ratings.rating, Ratings.user_id, Ratings.comments)\
+                        .join(Professors, Ratings.professor_id == Professors.professor_id)\
+                        .join(Courses, Ratings.course_id == Courses.course_id)\
+                        .filter(Ratings.rating_id == rating_id)\
+                        .first()
         return single_rating
 
     def get_ratings_by_prof_course(self,prof_id, course_id):
@@ -68,6 +72,8 @@ class RatingRepository:
         course = Courses.query.filter_by(subject=subject, course_num=course_num).first()
         course_id = course.course_id
         return course_id
+    
+
     
 
 
