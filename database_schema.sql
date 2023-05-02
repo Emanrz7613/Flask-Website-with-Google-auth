10753263ef165530e3c288cb17c890937d914207
Emanrz7613 <echaves@uncc.edu>
2023-05-02 14:40:46 -0400

    Insert test data added
    added insert statements to add test data to the database.

--- database_schema.sql
+++ database_schema.sql
@@ -29,7 +29,8 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
-
+INSERT INTO courses (subject, course_num)
+VALUES ('ITSC', '3155'), ('MATH', '1242'), ('ITSC', '1213'), ('ITIS', '3200');
 
 -- -----------------------------------------------------
 -- Table `3155_final_project`.`professors`
@@ -43,6 +44,8 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
+INSERT INTO professors (first_name, last_name)
+VALUES ('Professor', 'One'), ('Professor', 'Two'), ('Professor', 'Three');
 
 -- -----------------------------------------------------
 -- Table `3155_final_project`.`professor_course`
@@ -77,6 +80,9 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
+INSERT INTO users (first_name, last_name, email)
+VALUES ('Eric', 'Chaves', 'echaves@uncc.edu'), ('Ian', 'York', 'iyork@uncc.edu'), ('Ronnie', 'Johnston', 'rjohn249@uncc.edu'),
+		('Dylan', 'D''''Eloia', 'ddeloia@uncc.edu'), ('Gabriel', 'Van Dreel', 'gvandree@uncc.edu');
 
 -- -----------------------------------------------------
 -- Table `3155_final_project`.`ratings`
@@ -108,6 +114,14 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
+INSERT INTO ratings (user_id, course_id, professor_id, rating, semester, comments)
+VALUES (1, 1, 1, 4, 'Spring 2023', 'half of course grade was based on tests, great if you are a good test taker'),
+	(2,2,2,2, 'Fall 2022', 'I found this university class to be really engaging and thought-provoking - I learned so much!'),
+    (3,3,3,3,'Spring 2023', 'The professor for this university class was fantastic - they were really passionate about the subject matter.'),
+    (4,4,1,5,'Summer 2023', 'I was grateful for the opportunity to take this university class - it helped me develop new skills and perspectives.'),
+    (5,1,2,2,'Spring 2023', 'This university class was challenging at times, but the material was so interesting that it was worth the effort.'),
+    (1,2,3,1,'Fall 2022', 'The discussions we had in this university class were some of the most thought-provoking conversations I''''ve had in a long time'),
+    (2,3,1,4, 'Spring 2023', 'I was surprised at how applicable the material in this university class was to my everyday life.');
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
